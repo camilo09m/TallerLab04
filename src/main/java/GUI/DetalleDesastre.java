64c42a702a12c085d18d5afd350be9cc31da799a
camilo09m <c.nanco01@ufromail.cl>
2021-12-01 11:00:27 -0300

    tercer commit

--- src/main/java/GUI/DetalleDesastre.java
+++ src/main/java/GUI/DetalleDesastre.java
@@ -1,4 +1,53 @@
 package GUI;
 
-public class DetalleDesastre {
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class DetalleDesastre extends JFrame {
+    private DefaultTableModel model;
+    private JPanel tablePanel;
+    private JTable table;
+    private JPanel buttonPanel;
+
+    public DetalleDesastre(){
+        this.tablePanel = new JPanel(new FlowLayout());
+        add(tablePanel);
+        this.buttonPanel = new JPanel(new FlowLayout());
+        add(buttonPanel);
+        agregarBoton();
+        setTitle("Detalles de Desastres");
+        setDefaultProperties();
+    }
+    private void setDefaultProperties(){
+        setSize(800,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
 }
+public DefaultTableModel getModel() {
+        return model;
+    }
+    private void detalleDesaparecidos(){
+        this.model = new DefaultTableModel();
+        model.addColumn("Tipo de desastre Geofisico");
+        model.addColumn("Numero de desaparecidos");
+        this.table = new JTable(model);
+
+        JScrollPane jScrollPane = new JScrollPane(this.table);
+        this.tablePanel.add(jScrollPane);
+    }
+    private void agregarBoton(){
+
+
+        JButton addContactButton = new JButton("Mostrar personas desaparecidas");
+        addContactButton.addActionListener(e ->detalleDesaparecidos() );
+        this.buttonPanel.add(addContactButton);
+
+        JButton terminateProgramButton = new JButton("Cerrar");
+        terminateProgramButton.addActionListener(e -> System.exit(0));
+        this.buttonPanel.add(terminateProgramButton);
+}
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
+import GUI.DetalleDesastre;
+
 public class Main {
+    public static void main(String[] args) {
+        DetalleDesastre dd = new DetalleDesastre();
+    }
 }
